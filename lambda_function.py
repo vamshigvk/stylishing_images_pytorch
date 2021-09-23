ca565eb3ee8a5aaa142692bb5d6fd16b25201d07
Your Name <vamshikrishnagvk5@gmail.com>
2021-09-23 23:04:49 +0530

    testing uploads

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,15 @@
 import json
 import boto3
+import os
+import botocore
 
 from stylize import validate
 print('validate imported from stylize')
 print('Loading function')
 
 s3 = boto3.client('s3')
-#s3_resource = boto3.resource('s3')
+s3_resource = boto3.resource('s3')
+
 
 def lambda_handler(event, context):
     destination_bucket_name = 'csv2dynamodbvialambda'
@@ -16,22 +19,31 @@ def lambda_handler(event, context):
 
     # Bucket Name where file was uploaded
     source_bucket_name = event['Records'][0]['s3']['bucket']['name']
-    print("Bucket name is: ", source_bucket_name)
+    print("Bucket name is: ", source_bucket_name, "only")
     
     # Filename of object (with path)
     file_key_name = event['Records'][0]['s3']['object']['key']
-    print('File key name is: ', file_key_name)
+    print('File key name is: ', file_key_name, "only")
 
-    #Copy Source Object
-    #copy_source_object = {'Bucket': source_bucket_name, 'Key': file_key_name}
+    bucket = s3_resource.Bucket(source_bucket_name)
+
+    for s3_object in bucket.objects.all():  
+        try:
+            print('present key is:', s3_object.key)
+            path, filename = os.path.split(s3_object.key)
+            bucket.download_file(s3_object.key, "/tmp/" + filename)
+        except botocore.exceptions.ClientError as error:
+            if error.response['Error']['Code'] == "404":
+                print(f"The object {s3_object.key} does not exist")
+            else:
+                raise
 
     #Downloading newly added object to test/content/ folder
-    s3.download_file(source_bucket_name, file_key_name, "./test/content/"+file_key_name)
-    print("downloaded a new image with file name: ",file_key_name," bucket name: ", source_bucket_name)
+    #s3.download_file(source_bucket_name, file_key_name, "./test/content/"+file_key_name)
+    #bucket.download_file(s3_object.key, "/tmp/" + filename)
 
-    # S3 copy object operation
-    #s3_client.copy_object(CopySource=copy_source_object, Bucket=destination_bucket_name, Key=file_key_name)
-    
+    print("downloaded a new image with file name: ",file_key_name," bucket name: ", source_bucket_name, "only")
+
     #Validates the downloaded image from S3 source bucket and stores the output image at test/styled
     print('validate is about to trigger')
     validate("./test/content","./test/styled")
@@ -39,7 +51,7 @@ def lambda_handler(event, context):
 
     style_list = ['bayanihan','lazy','mosaic','starry','tokyo_ghoul','udnie','wave']
     for i in len(range(style_list)):
-        s3.upload_file( "./test/content/"+style_list[i]+file_key_name, destination_bucket_name, "stylized"+file_key_name)
+        s3.upload_file( "./test/styled/"+style_list[i]+'/'+file_key_name, destination_bucket_name, style_list[i]+'/'+file_key_name)
         print('Uploaded file from style: ',style_list[i])
 
     print('uploaded all the files to destination bucket')
