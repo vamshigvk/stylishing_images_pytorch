4e844fe83a5b3562ddbdbd094cc24cf2724a6a14
Your Name <vamshikrishnagvk5@gmail.com>
2021-09-23 23:42:01 +0530

    range len download location and validate location

--- lambda_function.py
+++ lambda_function.py
@@ -27,16 +27,15 @@ def lambda_handler(event, context):
 
     bucket = s3_resource.Bucket(source_bucket_name)
 
-    for s3_object in bucket.objects.all():  
-        try:
-            print('present key is:', s3_object.key)
-            path, filename = os.path.split(s3_object.key)
-            bucket.download_file(s3_object.key, "/tmp/" + filename)
-        except botocore.exceptions.ClientError as error:
-            if error.response['Error']['Code'] == "404":
-                print(f"The object {s3_object.key} does not exist")
-            else:
-                raise
+    try:
+        path, filename = os.path.split(file_key_name)
+        print('Key we are downloading is: ',filename)
+        bucket.download_file(file_key_name, "/tmp/" + filename)
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == "404":
+            print(f"The object {file_key_name} does not exist")
+        else:
+            raise
 
     #Downloading newly added object to test/content/ folder
     #s3.download_file(source_bucket_name, file_key_name, "./test/content/"+file_key_name)
@@ -46,13 +45,13 @@ def lambda_handler(event, context):
 
     #Validates the downloaded image from S3 source bucket and stores the output image at test/styled
     print('validate is about to trigger')
-    validate("./test/content","./test/styled")
+    validate("/tmp","./test/styled")
     print("validate is finished and stylized the image")
 
     style_list = ['bayanihan','lazy','mosaic','starry','tokyo_ghoul','udnie','wave']
-    for i in len(range(style_list)):
-        s3.upload_file( "./test/styled/"+style_list[i]+'/'+file_key_name, destination_bucket_name, style_list[i]+'/'+file_key_name)
-        print('Uploaded file from style: ',style_list[i])
+    for i in range(len(style_list)):
+        s3.upload_file( "./test/styled/"+style_list[i]+'/'+filename, destination_bucket_name, style_list[i]+'/'+filename)
+        print('Uploaded file from style: ',style_list[i],'file uploaded is: ',filename )
 
     print('uploaded all the files to destination bucket')
 
